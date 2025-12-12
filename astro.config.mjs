ae43e3c8edb38c5b5a5f2d31d43e4ce28f50f09a
gahyeonyun <134496720+gahyeonyun@users.noreply.github.com>
2025-12-13 00:26:56 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://gahyeonyun.github.io/",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
